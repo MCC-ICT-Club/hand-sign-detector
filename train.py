c6059490ad1551cffc0cb99b8dfd3ac1c39f422c
Rylan Meilutis <rylan.meilutis@gmail.com>
2024-09-12 12:02:00 -0400

    set memorygrowth

--- train.py
+++ train.py
@@ -102,6 +102,11 @@ def main():
         Dense(num_classes, activation='softmax')  # num_classes for the output layer
     ])
 
+    physical_devices = tf.config.list_physical_devices('GPU')
+    for device in physical_devices:
+        tf.config.experimental.set_memory_growth(device, True)
+
+
     # Compile the model
     model.compile(optimizer='adam',
                   loss='categorical_crossentropy',
