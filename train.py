7b31596e4fc27987d39e434e9edd11beb547592f
Rylan Meilutis <rylan.meilutis@gmail.com>
2024-09-12 13:09:15 -0400

    stop oom errors

--- train.py
+++ train.py
@@ -17,10 +17,6 @@ num_classes = 11
 use_bounding_boxes = False
 
 
-physical_devices = tf.config.list_physical_devices('GPU')
-for device in physical_devices:
-    tf.config.experimental.set_memory_growth(device, True)
-
 tf.config.run_functions_eagerly(True)
 
 
