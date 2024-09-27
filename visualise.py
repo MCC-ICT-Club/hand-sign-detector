cb4e3becd8bf5238e9b29678c1b5abd986b484bc
Rylan Meilutis <rylan.meilutis@gmail.com>
2024-09-27 02:42:50 -0400

    visualisations

--- visualise.py
+++ visualise.py
@@ -3,7 +3,7 @@ from tensorflow.keras.utils import plot_model
 
 
 def visualize_model(output_image_path):
-    model = keras.models.load_model('hand_sign_model.keras')
+    model = keras.models.load_model('hand_sign_model_final.keras')
 
     # Visualize the model architecture and save it to an image file
     plot_model(model, to_file=output_image_path, show_shapes=True, show_layer_names=True)
